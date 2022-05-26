47ac85b3464f0f5892e0601c13875ec8f44f0a59
Mayur Maheshwari <u0649996@utah.edu>
2022-05-26 17:39:12 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,2 @@
-alert('Hello')
+Shopify.Checkout.OrderStatus.addContentBox(`<h1>You may be interested in :</h1>
+<img src="https://cdn.shopify.com/s/files/1/0062/9587/9780/products/worlds-favourite-flavoured-instant-coffee-515828_1024x1024.jpg?v=1648563280" alt="Trulli" width="500" height="333">`)
